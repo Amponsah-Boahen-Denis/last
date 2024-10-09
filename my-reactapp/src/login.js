1253b7955cefd7c7a18fe46055f2b3a4c9d8d69c
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-10-09 06:15:33 -0700

    Update login.js

--- my-reactapp/src/login.js
+++ my-reactapp/src/login.js
@@ -17,7 +17,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/login', {
+      const response = await fetch('https://lastback.vercel.app/?vercelToolbarCode=n0xyhfXVTKNV-m3/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
