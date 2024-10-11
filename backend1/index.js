0e082c26fef80102e85cac6a52801060f984be37
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-10-11 04:53:51 -0700

    Update index.js

--- backend1/index.js
+++ backend1/index.js
@@ -65,24 +65,24 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 // JWT token generation function
-// const generateToken = (id) => {
-//   return jwt.sign({ id }, tok, { expiresIn: '10d' });
-// };
+const generateToken = (id) => {
+  return jwt.sign({ id }, tok, { expiresIn: '10d' });
+};
 
-// // Middleware to protect routes
-// const protect = async (req, res, next) => {
-//   const token = req.cookies.token;
-//   if (!token) {
-//     return res.status(401).json({ message: 'No token, not authorized' });
-//   }
-//   try {
-//     const decoded = jwt.verify(token, tok);
-//     req.user = await User.findById(decoded.id).select('-password');
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({ message: 'Not authorized' });
-//   }
-// };
+// Middleware to protect routes
+const protect = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: 'No token, not authorized' });
+  }
+  try {
+    const decoded = jwt.verify(token, tok);
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Not authorized' });
+  }
+};
 
 
 // const generateToken = (id) => {
