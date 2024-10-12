cf1bf575f897aeb30f8dc7dbfe3949ce8022699f
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-10-12 05:48:41 -0700

    Update password.js

--- my-reactapp/src/password.js
+++ my-reactapp/src/password.js
@@ -11,7 +11,6 @@ function AddPassword() {
   const [notes, setNotes] = useState('');
   const [message, setMessage] = useState('');
 
-
   const isTokenExpired = (token) => {
   const payload = JSON.parse(atob(token.split('.')[1]));
   return payload.exp * 1000 < Date.now();
