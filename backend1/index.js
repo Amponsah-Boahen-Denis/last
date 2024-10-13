17eddc02bd1641760866a9d26e2d02e40666e304
Amponsah  Denis Boahen <92126671+Amponsah-Boahen-Denis@users.noreply.github.com>
2024-10-12 23:47:03 -0700

    Update index.js

--- backend1/index.js
+++ backend1/index.js
@@ -174,7 +174,6 @@ app.post('/account', protect, async (req, res) => {
     res.status(500).json({ error: 'Error creating account' });
   }
 });
-
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
